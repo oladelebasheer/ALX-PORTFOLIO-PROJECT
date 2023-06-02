63e0b7e4c8b2390469d51dc41ad63b01ecaebb54
oladelebasheer <ajaoooladele@gmail.com>
2023-06-02 01:18:02 +0100

    commit

--- Backend/main.py
+++ Backend/main.py
@@ -5,12 +5,10 @@ from config import DevConfig
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 api=Api(app, doc='/docs')
-
 @api.route('/hello')
 class HelleResource(Resource):
     def get(self):
-        return {"message":"hello world"}
-     
+        return {"message":"hello world"}  
 if __name__ == '__name__':
     app.run()
     
